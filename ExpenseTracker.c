eab8aa1ce00602e9fd7df73cd54047eed41be724
Hardik210 <72506941+Hardik210@users.noreply.github.com>
2026-04-20 11:38:30 +0530

    Refactor Expense Tracker for dynamic budget input
    Updated the expense tracker to allow dynamic budget input and improved user interaction.

--- ExpenseTracker.c
+++ ExpenseTracker.c
@@ -21,25 +21,54 @@ int Bills;
 int Health;
 }Budget;
 
+
+
 int main(){
     printf("EXPENSE TRACKER\n");
-    
 
-    Budget B={2000,2000,6000,3000,2000};
+    char deci;
+    Budget B;
     Expense E;
+    int Salary=15000;
+    int totalBudget;
     int choice;
     FILE *fptr;
+
+while(1){
+    printf("Enter budget for Shopping:");
+    scanf("%d",&B.Shopping);
+    printf("Enter budget for Entertainment:");
+    scanf("%d",&B.Entertainment);
+    printf("Enter budget for General:");
+    scanf("%d",&B.General);
+    printf("Enter budget for Bills:");
+    scanf("%d",&B.Bills);
+    printf("Enter budget for Health:");
+    scanf("%d",&B.Health);
+
+    totalBudget=B.Shopping+B.Entertainment+B.General+B.Bills+B.Health;
+
+    if(totalBudget>Salary){
+        printf("Budget is inavalid\n");
+    }
+    else{
+        printf("Budget is fine\n");
+        break;
+    }
+}
+
     printf("1,Add Expense\n");
     printf("2,Show Expense\n");
     printf("3,Exit\n");
   
-
-
-    while(1){
+while(1){
+    printf("\n");
     printf("Enter the choice:");
     scanf("%d",&choice);
         switch(choice){
         case 1:
+        deci='Y';
+        while(deci=='Y'){
         printf("Select the Categeorie\n");
         printf("0-Shopping\n 1-Entertainment\n 2-General\n 3-Bills\n 4-Health\n");
         printf("Select:");
@@ -88,11 +117,24 @@ int main(){
             }
             break;
         }
+
+        printf("Remaining Budget\n");
+        printf("Amount left in Shopping:%d\n",B.Shopping);
+        printf("Amount left in Entertainment:%d\n",B.Entertainment);
+        printf("Amount left in General:%d\n",B.General);
+        printf("Amount left in Bills:%d\n",B.Bills);
+        printf("Amount left in Health:%d\n",B.Health);
+        printf("Total Amount Left:%d\n",B.Shopping+B.Entertainment+B.General+B.Bills+B.Health);
+
         fptr=fopen("expense.txt","a");
         if(fptr!=NULL){
         fprintf(fptr,"Category:%d\n Amount:%d\n",E.category,E.amount);
         fclose(fptr);
         }
+
+        printf("Do you wnat to continue YES(Y) OR NO(N):");
+        scanf(" %c",&deci);
+    }
         break;
         case 2:
         printf("Remaining Budget\n");
@@ -111,6 +153,6 @@ int main(){
         default:
         printf("Invalid Choice\n");
     }
-  }
+}
   return 0;
 }
